03e106ab409c494962465686d77af6ac1ba3aa65
khomkritAD <124819699+KKrit-ad@users.noreply.github.com>
2024-09-07 23:02:44 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,10 +12,7 @@ const port = process.env.PORT || 5000; // ใช้ค่าจาก .env
 
 app.use(
   cors({
-    origin: [
-      process.env.FRONTEND_URL,
-      "https://aireagle-flightbooking-gogh6n94a-sinnermanxs-projects.vercel.app",
-    ], // เพิ่ม Vercel URL
+    origin: [process.env.FRONTEND_URL, "aireagle-flightbooking.vercel.app"], // เพิ่ม Vercel URL
     methods: "GET,POST,PUT,DELETE",
     credentials: true, // ถ้าคุณต้องการอนุญาต cookies
   })
